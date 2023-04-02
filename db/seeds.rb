da1e5d6ba6faa0f39a9cbc2a3c900433cb233f76
Todd Kummer <todd@rockridgesolutions.com>
2023-04-02 11:05:08 -0700

    Add Roy G Biv seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,8 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+list = TodoList.create!(name: 'Roy G Biv')
+
+%w[Red Orange Yellow Green Blue Indigo Violet].each_with_index do |color, index|
+  list.tasks.create!(description: color, sort_order: index)
+end
